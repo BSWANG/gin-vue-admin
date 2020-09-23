1683b647718a1355896352fe2d1098fa13884fa9
bingshen.wbs <bingshen.wbs@alibaba-inc.com>
2020-09-23 16:13:02 +0800

    sperate frontend static resource and backend api service

--- docker/docker-start.sh
+++ docker/docker-start.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 envsubst '$API_SERVER' < /etc/nginx/nginx.conf.tpl > /etc/nginx/nginx.conf
+if [ x"$1" == x"frontend" ]; then
+    env nginx
+    tail -f /var/log/nginx/access.log
+fi
 env nginx
 ./gvadmin
